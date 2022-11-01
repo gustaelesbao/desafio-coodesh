55baaab4686b52dbc7e9a071b650a09b160760c7
Gustavo Elesbão <elesbao.gustavo321@gmail.com>
2022-11-01 01:04:33 -0300

    chore: project initialization

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+const Page404 = () => {
+  return (
+    <>
+      <h1>404</h1>
+      <p>Página não encontrada</p>
+    </>
+  );
+};
+
+export default Page404;
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name='description' content='Desafio Coodesh' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main>
+        <h1>Olá mundo!</h1>
+      </main>
+    </>
+  );
+}
