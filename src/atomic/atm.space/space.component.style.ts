7a2dc743445251f245b0164749e46bc6b9b469b7
Gustavo Elesbão <elesbao.gustavo321@gmail.com>
2022-11-01 11:24:06 -0300

    feat: create space component

--- src/atomic/atm.space/space.component.style.ts
+++ src/atomic/atm.space/space.component.style.ts
@@ -1,20 +1,10 @@
-import { Property } from 'csstype';
 import styled from 'styled-components';
 
 import { Spacing } from 'app/constants';
 import { doubleResponsiveProps } from 'utils/resposive-props';
-interface DividerProps {
-  size?: Property.Padding;
+import { SpaceStyledProps } from './space.d';
 
-  sizeXxl?: Property.Padding;
-  sizeXl?: Property.Padding;
-  sizeLg?: Property.Padding;
-  sizeMd?: Property.Padding;
-  sizeSm?: Property.Padding;
-  sizeXs?: Property.Padding;
-}
-
-export const Divider = styled.div<DividerProps>`
+export const SpaceStyled = styled.div<SpaceStyledProps>`
   padding-top: ${(props) => (props.size ? props.size : Spacing.Size6X)};
   padding-left: ${(props) => (props.size ? props.size : Spacing.Size6X)};
 
